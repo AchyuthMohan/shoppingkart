022c76d1f37df9de09f4649931a50748add179f3
Achyuth Mohan <achyuthmohan1@gmail.com>
2021-11-07 22:31:29 +0530

    initil commit

--- shoppingkart/settings.py
+++ shoppingkart/settings.py
@@ -81,8 +81,8 @@ WSGI_APPLICATION = 'shoppingkart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'shoppingkart',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
